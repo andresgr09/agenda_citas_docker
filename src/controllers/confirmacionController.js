bf00bf0dc37b3be16d26dae7ea8d6b614430f614
Andres Felipe Granados Cano <andres.granados@migracioncolombia.gov.co>
2025-03-13 11:27:25 -0500

    Actualiza el mensaje de error en la confirmación de cita para indicar que el enlace ha expirado.

--- src/controllers/confirmacionController.js
+++ src/controllers/confirmacionController.js
@@ -173,7 +173,7 @@ export const confirmarCita = async (req, res) => {
                     <div id="modal" style="display: block; position: fixed; z-index: 1; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgb(0,0,0); background-color: rgba(0,0,0,0.4);">
                         <div style="background-color: #fefefe; margin: 15% auto; padding: 20px; border: 1px solid #888; width: 80%;">
                             <img src="/citas/images/logo-migracion-colombia.png" alt="Logo" style="width: 250px; display: block; margin: 0 auto;">
-                            <h1 style="color: red; text-align: center;">Error al confirmar la cita</h1>
+                            <h1 style="color: red; text-align: center;">El enlace de confirmación ha expirado</h1>
                             <button onclick="window.close()" style="display: block; margin: 20px auto; padding: 10px 20px; background-color: #f44336; color: white; border: none; cursor: pointer;">Cerrar ventana</button>
                         </div>
                     </div>
